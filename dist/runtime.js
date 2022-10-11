19b9059f4e3dc56c68ddecf0e192e920e0d9fd76
joker69216 <hamaraad883@gmail.com>
2022-10-11 20:47:45 +0300

    setup deployment branch

--- dist/runtime.js
+++ dist/runtime.js
@@ -158,7 +158,7 @@
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkweb_template"] = self["webpackChunkweb_template"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunkleaderboard"] = self["webpackChunkleaderboard"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
